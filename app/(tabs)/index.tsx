cd0b7998e15924778d651b2f32a91a33322ebbe5
pxuria <pouriamesram@gmail.com>
2025-11-11 18:59:13 +0330

    Add Tabs

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className='flex-1 items-center justify-center'>
+      <Text className="text-4xl font-bold text-dark-200">HELLO</Text>
+    </View>
+  );
+}
