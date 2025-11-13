a08436cd31697acfc4eaf789eda2e42791425f91
pxuria <pouriamesram@gmail.com>
2025-11-14 00:50:35 +0330

    Create movies cards

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,16 @@
+import MovieCard from "@/components/MovieCard";
 import Searchbar from "@/components/Searchbar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/use-fetch";
 import { useRouter } from "expo-router";
-import { Image, ScrollView, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
   const router = useRouter();
+
+  const { data: movies, loading, error } = useFetch(() => fetchMovies({ query: '' }));
 
   return (
     <View className='flex-1 bg-primary'>
@@ -16,6 +21,40 @@ export default function Index() {
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ minHeight: '100%', paddingBottom: 10 }}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+        {loading ? (
+          <ActivityIndicator
+            size='large'
+            color='#0000ff'
+            className="mt-10 self-center" />
+        ) : (
+          error ? (
+            <Text>Error:{error?.message}</Text>
+          ) : (
+            <View className="flex-1 mt-5">
+              <Searchbar
+                onPress={() => router.push('/search')}
+                placeholder="Search for a movie..." />
+              <>
+                <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
+                <FlatList
+                  data={movies}
+                  numColumns={3}
+                  scrollEnabled={false}
+                  keyExtractor={(item) => item.id}
+                  columnWrapperStyle={{
+                    justifyContent: 'flex-start',
+                    gap: 20,
+                    paddingRight: 5,
+                    marginBottom: 10
+                  }}
+                  className="mt-2 pb-32"
+                  renderItem={({ item }) => <MovieCard {...item} />}
+                />
+              </>
+            </View>
+          )
+        )}
 
         <View className="flex-1 mt-5">
           <Searchbar
